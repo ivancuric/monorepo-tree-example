922765bf28749e37ce968224625f51a6d2e1c580
Ivan Čurić <ivan.curic@gmail.com>
2022-06-15 11:56:12 +0200

    comment

--- packages/A/src/index.ts
+++ packages/A/src/index.ts
@@ -2,3 +2,10 @@ import { A } from "@example/core";
 export { A };
 
 export type TypeA = typeof A;
+
+/**
+ * Would it be possible to somehow import everything and pick exports?
+ */
+
+// import * as core from "@example/core";
+// export {core.A, typeof A}
